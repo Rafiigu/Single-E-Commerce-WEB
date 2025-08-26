05268960426605d06743744bf186e8dac8b6a43f
Jayaku Briliantio <jayakubriliantio@gmail.com>
2025-08-26 17:39:08 +0700

    feat: basic app page structure

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const AppLayout = ({ children }: Props) => {
+  return <>{children}</>;
+};
+
+export default AppLayout;
